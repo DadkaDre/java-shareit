3fae1fc79513d5098f49745544036bdf693f4607
Дядька <mr.kotman@list.ru>
2024-09-25 17:38:34 +0300

    Последнее исправления

--- src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
@@ -3,13 +3,15 @@ package ru.practicum.shareit.exception;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Data;
-import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ErrorResponse {
     @NotBlank
-    String error;
-    String description;
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
 }
